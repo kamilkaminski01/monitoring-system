b9c52432c08b5723108471c2c2ae1dc5becf2240
Kamil Kaminski <kamilkaminski39@gmail.com>
2022-12-02 16:21:34 +0100

    Added frontend files

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Board from "./components/Board";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Board />
+  </React.StrictMode>
+);
+
+
