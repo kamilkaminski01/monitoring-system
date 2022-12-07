0a87067d0a04c8cc26a3997a93a63486b7907c18
Kamil Kaminski <kamilkaminski39@gmail.com>
2022-12-07 01:13:16 +0100

    Added a second card component to home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,14 +4,24 @@ import "./css/App.css";
 
 function Home() {
   return (
-    <div className="App">
-      <h1>Monitoring System</h1>
-      <Card
-        title="Whiteboard"
-        imageUrl="https://picsum.photos/300"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link="http://localhost:3000/whiteboard"
-      />
+    <div>
+      <div className="title">
+        <h1>Monitoring system</h1>
+      </div>
+      <div className="App">
+        <Card
+          title="Whiteboard"
+          imageUrl="https://picsum.photos/300"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          link="http://localhost:3000/whiteboard"
+        />
+        <Card
+          title="Bingo"
+          imageUrl="https://picsum.photos/300"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          link="http://localhost:3000/bingo"
+        />
+      </div>
     </div>
   );
 }
