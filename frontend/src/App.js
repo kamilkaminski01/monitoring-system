210982535489559452df8be8c3b36971090f8c08
Kamil Kaminski <kamilkaminski@bluerider.software>
2022-12-07 10:55:58 +0100

    Added route paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { PATHS } from "./utils/consts";
 import Home from "./components/Home";
+import Whiteboard from "./components/Whiteboard";
+import "./components/css/App.css";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Routes>
+        <Route path={PATHS.home} element={<Home />} />
+        <Route path={PATHS.whiteboard} element={<Whiteboard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/utils/consts.js
@@ -0,0 +1,4 @@
+export const PATHS = {
+  home: "/",
+  whiteboard: "/whiteboard",
+};
