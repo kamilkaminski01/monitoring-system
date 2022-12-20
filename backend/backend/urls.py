d97353f3a1f946002a92b1fc7c6c4807c580a3fc
Kamil Kaminski <kamilkaminski@bluerider.software>
2022-12-20 13:46:59 +0100

    Added type hints and refactored code

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("bingo/", include("bingo.urls")),
     path("admin/", admin.site.urls),
+    path("bingo/", include("bingo.urls")),
 ]
--- backend/whiteboard/consumers.py
+++ backend/whiteboard/consumers.py
@@ -3,7 +3,7 @@ from channels.exceptions import StopConsumer
 
 
 class BoardConsumer(AsyncConsumer):
-    async def websocket_connect(self, event):
+    async def websocket_connect(self, event: dict) -> None:
         self.board_room = "boardroom"
         await self.channel_layer.group_add(self.board_room, self.channel_name)
         await self.send(
@@ -12,15 +12,15 @@ class BoardConsumer(AsyncConsumer):
             }
         )
 
-    async def websocket_receive(self, event):
+    async def websocket_receive(self, event: dict) -> None:
         initial_data = event.get("text", None)
         await self.channel_layer.group_send(
             self.board_room, {"type": "board_message", "text": initial_data}
         )
 
-    async def board_message(self, event):
+    async def board_message(self, event: dict) -> None:
         await self.send({"type": "websocket.send", "text": event["text"]})
 
-    async def websocket_disconnect(self, event):
+    async def websocket_disconnect(self, event: dict) -> None:
         await self.send({"type": "websocket.close"})
         raise StopConsumer()
