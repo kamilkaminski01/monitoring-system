7d29b1668e167e80f1f8caaf98ac5d97edff7634
Kamil Kaminski <kamilkaminski39@gmail.com>
2022-12-26 18:49:34 +0100

    Added basic footer

--- frontend/src/components/organisms/Footer/Footer.js
+++ frontend/src/components/organisms/Footer/Footer.js
@@ -1,7 +1,10 @@
+import './Footer.css';
+
 export default function Footer() {
   return (
-    <div>
-      <p></p>
-    </div>
+    <footer>
+      <p>Monitoring system for interactive web applications</p>
+      <span>Kamil Kaminski 2022</span>
+    </footer>
   );
 }
